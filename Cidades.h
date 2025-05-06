0f2f979e4338ac02334e04d9fb6452c4e0955898
Kauan-limaa <kauamlimaaa@gmail.com>
2025-05-06 14:11:23 -0300

    Update Cidades.h
    tornei os get constantes pois vamos utilizar eles em outras classes

--- Cidades.h
+++ Cidades.h
@@ -4,15 +4,15 @@ class Cidades{
         int cod_cidade;
         string descricao_cidade;
         string uf;
-        
+
     public:
         Cidades(): cod_cidade(0), descricao_cidade(""), uf(""){}
         Cidades(int, string, string);
-        
-        int getcod_cidade(){return this -> cod_cidade;}
-        string getdescricao_cidade(){return this -> descricao_cidade;}
-        string getuf(){return this -> uf;}
-        
+
+        int getcod_cidade()const {return this -> cod_cidade;}
+        string getdescricao_cidade()const {return this -> descricao_cidade;}
+        string getuf()const {return this -> uf;}
+
         void setcod_cidade(int _cod_cidade){this -> cod_cidade = _cod_cidade;}
         void setdescricao_cidade(string _descricao_cidade){this -> descricao_cidade = _descricao_cidade;}
         void setuf(string _uf){this -> uf = _uf;}
