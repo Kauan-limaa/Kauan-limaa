17cd985e11701251a48e9ed894414b876a84a678
Ana Messa <164221120+anm3ssa@users.noreply.github.com>
2025-05-07 14:18:27 -0300

    Update Autores.h

--- Autores.h
+++ Autores.h
@@ -6,11 +6,11 @@ public:
   Autores(): cod_autor(0), nome("") {};
   Autores(int cod_autor, string nome);
 
-  int get_cod_autor() {return cod_autor;};
+  int get_cod_autor() const {return cod_autor;};
   string get_nome() const {return nome;}
 
   void set_cod_autor(int cod_autor){this->cod_autor = cod_autor;}
-  void set_nome(string nome) {this->nome = nome;};
+  void set_nome(string& nome) {this->nome = nome;};
 
 };
 
@@ -18,4 +18,3 @@ public:
   this->cod_autor = cod_autor;
   this->nome = nome;
 }
-
