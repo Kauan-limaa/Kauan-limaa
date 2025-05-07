7fad4d1b50e4953214c722ba73272f3040dd1a7d
Ana Messa <164221120+anm3ssa@users.noreply.github.com>
2025-05-07 14:17:21 -0300

    Update Generos.h

--- Generos.h
+++ Generos.h
@@ -1,18 +1,22 @@
+#include <string>
 
-class Generos{
-    private:
-        int cod_genero;
-        string descricao_genero;
+class Generos {
+private:
+    int cod_genero;
+    string descricao_genero;
 
-    public:
-        Generos():cod_genero(0), descricao_genero(""){}
-        Generos(int _cod_genero, string _descricao_genero):cod_genero(_cod_genero),descricao_genero(_descricao_genero){}
+public:
+    Generos() : cod_genero(0), descricao_genero("") {}
+    Generos(int cod_genero, const string& descricao_genero);
 
-        int getcod_genero(){return this -> cod_genero;}
-        string getdescricao_genero(){return this -> descricao_genero;}
+    int getcod_genero() const { return cod_genero; }
+    string getdescricao_genero() const { return descricao_genero; }
 
-        void setcod_genero(int _cod_genero){this -> cod_genero = _cod_genero;}
-        void setdescricao_genero(string _descricao_genero){this -> descricao_genero = _descricao_genero;}
-
+    void setcod_genero(int _cod_genero) { this->cod_genero = _cod_genero; }
+    void setdescricao_genero(const string& _descricao_genero) { this->descricao_genero = _descricao_genero; }
 };
 
+Generos::Generos(int cod_genero, const string& descricao_genero) {
+    this->cod_genero = cod_genero;
+    this->descricao_genero = descricao_genero;
+}
