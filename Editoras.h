72e920e69dd7d1c4fbb91b1369469a8bcb79bdfd
Kauan-limaa <kauamlimaaa@gmail.com>
2025-05-08 11:37:16 -0300

    Update Editoras.h
    passei os gets para const

--- Editoras.h
+++ Editoras.h
@@ -1,24 +1,26 @@
-class Editoras {
-private:
-    int cod_editora;
-    string nome_editora;
-    int cod_cidade;
 
-public:
-    Editoras() : cod_editora(0), nome_editora(""), cod_cidade(0) {}
-    Editoras(int cod_editora, const string& nome_editora, int cod_cidade);
+class Editoras{
+    private:
+        int cod_editora;
+        string nome_editora;
+        int cod_cidade;
 
-    int getcod_editora() const { return cod_editora; }
-    string getnome_editora() const { return nome_editora; }
-    int getcod_cidade() const { return cod_cidade; }
+    public:
+        Editoras(): cod_editora(0), nome_editora(""), cod_cidade(0){}
+        Editoras(int, string, int);
 
-    void setcod_editora(int _cod_editora) { this->cod_editora = _cod_editora; }
-    void setnome_editora(const string& _nome_editora) { this->nome_editora = _nome_editora; }
-    void setcod_cidade(int _cod_cidade) { this->cod_cidade = _cod_cidade; }
+        int getcod_editora()const {return this -> cod_editora;}
+        string getnome_editora()const {return this -> nome_editora;}
+        int getcod_cidade()const {return this -> cod_cidade;}
+
+        void setcod_editora(int _cod_editora){this -> cod_editora = _cod_editora;}
+        void setnome_editora(string _nome_editora){this -> nome_editora = _nome_editora;}
+        void setcod_cidade(int _cod_cidade){this -> cod_cidade = _cod_cidade;}
+
 };
 
-Editoras::Editoras(int cod_editora, const string& nome_editora, int cod_cidade) {
-    this->cod_editora = cod_editora;
-    this->nome_editora = nome_editora;
-    this->cod_cidade = cod_cidade;
+Editoras::Editoras(int _cod_editora, string  _nome_editora, int _cod_cidade){
+    this -> cod_editora = _cod_editora;
+    this -> nome_editora = _nome_editora;
+    this -> cod_cidade = _cod_cidade;
 }
