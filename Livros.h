c80ad6576f8a4cb06bc7c1cced90bb0155e3b472
Kauan-limaa <kauamlimaaa@gmail.com>
2025-05-15 18:09:37 -0300

    Update Livros.h

--- Livros.h
+++ Livros.h
@@ -10,12 +10,12 @@ private:
     Livros(): cod_livro(0), nome(""), cod_editora(0), cod_autor(0), cod_genero(0), disponivel("")  {}
     Livros(int cod_livros, string nome, int cod_editora, int cod_autor, int cod_genero, string disponivel);
 
-    int getCod_livros(){return cod_livro;}
-    string getNome(){return nome;}
-    int getCod_editora(){return cod_editora;}
-    int getCod_autor(){return cod_autor;}
-    int getCod_genero(){return cod_genero;}
-    string getDisponivel() { return disponivel; }
+    int getCod_livros()const {return cod_livro;}
+    string getNome()const {return nome;}
+    int getCod_editora()const {return cod_editora;}
+    int getCod_autor()const {return cod_autor;}
+    int getCod_genero()const {return cod_genero;}
+    string getDisponivel() const { return disponivel; }
 
     void setCod_livros(int cod_livro){ this-> cod_livro = cod_livro;}
     void setNome(string nome){this->nome = nome;}
