c50fd0075fe50b8cdc6212a829a5d242f0492271
Ana Messa <164221120+anm3ssa@users.noreply.github.com>
2025-05-30 13:02:55 -0300

    Update Cidades.h

--- Cidades.h
+++ Cidades.h
@@ -1,25 +1,25 @@
 
 class Cidades{
-    private:
-        int cod_cidade;
-        string descricao_cidade;
-        string uf;
+private:
+    int cod_cidade;
+    string descricao_cidade;
+    string uf;
 
-    public:
-        Cidades(): cod_cidade(0), descricao_cidade(""), uf(""){}
-        Cidades(int, string, string);
+public:
+    Cidades(): cod_cidade(0), descricao_cidade(""), uf(""){}
+    Cidades(int, string, string);
 
-        int getcod_cidade()const {return this -> cod_cidade;}
-        string getdescricao_cidade()const {return this -> descricao_cidade;}
-        string getuf()const {return this -> uf;}
+    int getcod_cidade()const {return this -> cod_cidade;}
+    string getdescricao_cidade()const {return this -> descricao_cidade;}
+    string getuf()const {return this -> uf;}
 
-        void setcod_cidade(int _cod_cidade){this -> cod_cidade = _cod_cidade;}
-        void setdescricao_cidade(string _descricao_cidade){this -> descricao_cidade = _descricao_cidade;}
-        void setuf(string _uf){this -> uf = _uf;}
+    void setcod_cidade(int _cod_cidade){this -> cod_cidade = _cod_cidade;}
+    void setdescricao_cidade(string _descricao_cidade){this -> descricao_cidade = _descricao_cidade;}
+    void setuf(string _uf){this -> uf = _uf;}
 };
 
- Cidades::Cidades(int _cod_cidade, string _descricao_cidade, string _uf){
+Cidades::Cidades(int _cod_cidade, string _descricao_cidade, string _uf){
     this -> cod_cidade = _cod_cidade;
     this -> descricao_cidade = _descricao_cidade;
     this -> uf = _uf;
- }
+}
