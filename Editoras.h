55d39f17e4f00e8d5e21b5f3abe3c460cb2aeaa3
Ana Messa <164221120+anm3ssa@users.noreply.github.com>
2025-05-30 13:03:28 -0300

    Update Editoras.h

--- Editoras.h
+++ Editoras.h
@@ -1,21 +1,21 @@
 
 class Editoras{
-    private:
-        int cod_editora;
-        string nome_editora;
-        int cod_cidade;
+private:
+    int cod_editora;
+    string nome_editora;
+    int cod_cidade;
 
-    public:
-        Editoras(): cod_editora(0), nome_editora(""), cod_cidade(0){}
-        Editoras(int, string, int);
+public:
+    Editoras(): cod_editora(0), nome_editora(""), cod_cidade(0){}
+    Editoras(int, string, int);
 
-        int getcod_editora()const {return this -> cod_editora;}
-        string getnome_editora()const {return this -> nome_editora;}
-        int getcod_cidade()const {return this -> cod_cidade;}
+    int getcod_editora()const {return this -> cod_editora;}
+    string getnome_editora()const {return this -> nome_editora;}
+    int getcod_cidade()const {return this -> cod_cidade;}
 
-        void setcod_editora(int _cod_editora){this -> cod_editora = _cod_editora;}
-        void setnome_editora(string _nome_editora){this -> nome_editora = _nome_editora;}
-        void setcod_cidade(int _cod_cidade){this -> cod_cidade = _cod_cidade;}
+    void setcod_editora(int _cod_editora){this -> cod_editora = _cod_editora;}
+    void setnome_editora(string _nome_editora){this -> nome_editora = _nome_editora;}
+    void setcod_cidade(int _cod_cidade){this -> cod_cidade = _cod_cidade;}
 
 };
 
